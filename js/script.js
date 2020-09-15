80da88423583d2ad3e54d95a8391f89c3bd997cc
VadimMir <vmironov1@gmail.com>
2020-09-15 12:32:52 +0300

    Changed

--- js/script.js
+++ js/script.js
@@ -2,5 +2,19 @@
 
 //alert('Hello');
 
-const result = prompt("Напишите", "фывфыв");
-console.log(typeof(result));
+// const result = prompt("Напишите", "фывфыв");
+// console.log(typeof(result));
+
+
+
+
+
+let incr = 10,
+    decr = 10;
+
+
+
+    incr++;
+
+
+    decr--;
