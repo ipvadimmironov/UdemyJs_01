94c4d5ee158835d993606d259d1e3e40fbc586c0
Vadim <vmironov1@gmail.com>
2020-09-18 14:35:07 +0300

    second practice end

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ let numberOfFilms;
 
 numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
 
+if (numberOfFilms<10) {console.log("Просмотрено мало фильмов");}
+else if (numberOfFilms>=10 && numberOfFilms <30) {console.log("Все ок");} 
+else if (numberOfFilms>=30 ) {console.log("Вы кионман");} 
+else {console.log("error");}
+
+
 console.log(numberOfFilms);
 
 let personalMovieDB = {
@@ -16,7 +22,14 @@ let personalMovieDB = {
 
 
 for (let cnt = 0; cnt < personalMovieDB.count; cnt++) {
-    let movieName = prompt("Назовите фильм");
+    let movieName="";
+    do {
+        movieName = prompt("Назовите фильм") || "";
+    }
+    while (movieName.length == 0 || movieName.length > 50);
+    
+    
+    
     let rank = +prompt("Поставьте оценку");
 
     personalMovieDB.movies[movieName] = rank;
