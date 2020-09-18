5190254a4db7f9d3aab59cf18643c49a341f84bc
Vadim CarbonX1 <vmironov1@gmail.com>
2020-09-18 14:05:23 +0300

    duplicate script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,3 @@
 "use strict";
 
-let numberOfFilms;
 
-numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
-
-console.log(numberOfFilms);
-
-let personalMovieDB = {
-    count:numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat:false
-};
-
-
-for (let cnt = 0; cnt < personalMovieDB.count; cnt++) {
-    let movieName = prompt("Назовите фильм");
-    let rank = +prompt("Поставьте оценку");
-
-    personalMovieDB.movies[movieName] = rank;
-
-}
-
-console.log(personalMovieDB);
